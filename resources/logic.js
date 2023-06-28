cd90157217dae2a4ffbb1cf8feb76b94657e55a6
MiguelBelo95 <belo_miguel@hotmail.com>
2023-06-28 16:01:21 +0100

    Operations working and displalying on currentScreen. Missing C and AC buttons

--- resources/logic.js
+++ resources/logic.js
@@ -1,5 +1,5 @@
-let first_num = 0
-let second_num = 0
+let firstNumber = 0
+let secondNumber = 0
 let operator = ''
 
 
@@ -8,7 +8,7 @@ const screenCurrent = document.getElementById("screen--current")
 
 const buttons = document.querySelectorAll(".btn")
 
-screenCurrent.innerText = first_num
+screenCurrent.innerText = firstNumber
 screenLast.innerText = "Let's do some Maths"
 
 
@@ -18,13 +18,57 @@ screenLast.innerText = "Let's do some Maths"
 buttons.forEach( btn => {
 	btn.addEventListener("click", () => {
 		const btnText = btn.innerText;
-		if (second_num == 0 && operator == '') {
-			first_num = first_num * 10 + btnText
-		}
-		console.log(btnText);
-	})
+		
+		// Check if the button clicked is a number
+		if (!isNaN(btnText) || btnText === '.') {
+			// Check if the operator has been selected
+			if (operator === '') {
+			  // Add the clicked button text to the first number
+			  firstNumber = updatedNum(btnText, firstNumber);
+			  updateScreen(firstNumber, screenCurrent);
+			} else {
+			  // Add the clicked button text to the second number
+			  secondNumber = updatedNum(btnText, secondNumber);
+			  updateScreen(firstNumber, screenLast);
+			  updateScreen(secondNumber, screenCurrent);
+			}
+			}
+		  // Check if the button clicked is an operator
+		  else if (btn.classList.contains('operator')) {
+			if (secondNumber) {
+				console.log("IM herreeeeee!!!")
+				operate(firstNumber, secondNumber, operator);
+				secondNumber = 0;
+			}
+			operator = btnText;
+			screenCurrent.innerText = operator;
+		  }
+	  
+		  console.log('First Number:', firstNumber);
+		  console.log('Operator:', operator);
+		  console.log('Second Number:', secondNumber);
+		});
 })
 
+function updatedNum(btnText, num) {
+	// Add the clicked button text to the first number
+	if (btnText === '.' && !num.includes('.')) {
+		// Add decimal point only if it hasn't been added before
+		num += btnText;
+	  } else if (btnText !== '.') {
+		// Append the clicked number to the first number
+			if (num === 0) {
+				num = btnText;
+			} else {
+				num += btnText;
+			}
+		}
+	return num
+}
+
+function updateScreen(btnText,screen) {
+	screen.innerText = btnText;
+}
 
 /* CALCULATOR OPERATIONS */
 
@@ -50,6 +94,34 @@ function modulus(first, second) {
 
 function operate(first, second, operator) {
 	/* takes an operator and 2 numbers and then calls one of the above functions on the numbers */
+	total = 0;
+	firstNum = parseFloat(first);
+	secondNum = parseFloat(second);
+	switch(operator) {
+		case 'X':
+			total = multiply(firstNum, secondNum);
+			break;
+		case '/':
+			// code block
+			total = divide(firstNum, secondNum);
+			break;
+		case '-':
+			// code block
+			total = subtract(firstNum, secondNum);
+			break;
+		case '+':
+			// code block
+			total = sum(firstNum, secondNum);
+			break;
+		case '%':
+			// code block
+			total = modulus(firstNum, secondNum);
+			break;
+		default:
+			console.log('Invalid operator');
+	}
+	firstNumber = total;
+	updateScreen(firstNumber,screenLast);
 }
 
 function clearAll() {}
